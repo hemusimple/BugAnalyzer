3f94313add221fa85779f6a09bcc5edaec3a780a
hemusimple <hemanth9320@gmail.com>
2026-03-20 20:12:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.test
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val data = 10
+        Log.i("LoginActivity","login button clicked")
+        Log.i("LoginRepository","send login details")
+        Log.i("LoginService","receive login details, user hey there ${data}")
+        Log.i("LoginService","send login response")
+        Log.i("LoginRepository","receive login response")
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
